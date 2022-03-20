9285ad641a2f4415a69c6b79e98f499b3be40eb2
KauanCarvalho <kauan_calasans@hotmail.com>
2022-03-20 00:21:02 -0300

    Revert

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-# rubocop:disable Lint/EmptyClass
-class ApplicationMailer
-  # default from: 'from@example.com'
-  # layout 'mailer'
+class ApplicationMailer < ActionMailer::Base
+  default from: 'from@example.com'
+  layout 'mailer'
 end
-# rubocop:enable Lint/EmptyClass
